c24617e17fd8ea1a329ddaa00f674d75cc1259a2
saintmatthew-svg <onyekachimatthew00@gmail.com>
2025-01-05 21:04:32 +0100

    saved filescls

---
+++ Desktop/CODES/dot.java
@@ -0,0 +1,23 @@
+
+public class dot {
+	public static void main(String[] args){
+	
+	for(int even = 2; even <= 10; even = even + 2){
+	System.out.println(even);
+
+int sumofeven = even + even;{
+System.out.println("SUM" +sumofeven);}
+}
+
+	
+	for(int odd = 1; odd <= 10; odd = odd + 2){
+	System.out.println(odd);
+
+int sumofodd = odd + odd;{
+System.out.println("SUM" +sumofodd);}
+}
+}
+
+
+
+}
