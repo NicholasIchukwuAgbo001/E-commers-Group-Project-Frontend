2793642c5fa00aa362eb20c22d4eadc07591656c
saintmatthew-svg <onyekachimatthew00@gmail.com>
2025-01-15 15:53:31 +0100

    saved files

--- Documents/python.py/Phase Gate One/TaskEight.py
+++ Documents/python.py/Phase Gate One/TaskEight.py
@@ -8,14 +8,15 @@ for question in range(10):
     numbertwo = random.randrange(1,1000)
     print(numberone, '-', numbertwo)
     answer = int(input('ENTER THE CORRECT ANSWER FOR THE ABOVE: '))
-    result = ('incorrect' if answer != (numberone - numbertwo) else 'correct')
-    print(result)
-    print()
-    if result == 'incorrect':
+    if answer != (numberone - numbertwo):
         incorrect+1
-    if result == 'correct':
+        print('incorrect')
+        print()
+    if answer == (numberone - numbertwo):
         correct+1
-        
-print('passed', correct + correct)
-print('failed', incorrect + incorrect)
+        print('correct')
+        print()
+                
+print('passed', correct)
+print('failed', incorrect)
         
